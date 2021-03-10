f94fa335ed1ec456dccaf748761010afea3a1230
nyrivera <68795543+nyrivera@users.noreply.github.com>
2021-03-09 16:52:40 -0800

    Add files via upload

---
+++ LA9.java
@@ -0,0 +1,32 @@
+// Nelson Rivera
+
+public class LA9 {
+	
+	public static double findHypotenuse(double a, double b) {
+		double hypotenuse =0;
+		hypotenuse = Math.sqrt(Math.pow(a , 2)+ (Math.pow(b , 2)));
+		return hypotenuse;
+	}
+	
+	public static int quadrant(double x, double y) {
+		if(x>=1 && y>=1) {
+			return 1;
+		}else if(x<=-1 && y>=1){
+			return 2;
+		}else if(x<=-1 && y <= -1) {
+			return 3;
+		}else if( x>=1 && y<=-1) {
+			return 4;
+		}
+			return 0;
+	}
+	public static void main(String[] args) {
+		double a = 5.0;
+		double b = 5.0;
+		double x = 4.5;
+		double y = -5;
+		System.out.println(findHypotenuse(a,b));
+		System.out.println("Your coordinates are on quadrant " + quadrant(x,y) + ".");
+	}
+
+}
